edc8ff3b7c983855e36798d7b0565511880f6acb
Vinit Hitesh Harsora <vinithiteshharsora@vinits-air.myfiosgateway.com>
2021-09-26 16:24:14 -0400

    Lab 1

---
+++ Lab1/src/model/Product.java
@@ -0,0 +1,171 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.File;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author vinithiteshharsora
+ */
+public class Product {
+    
+    private String name;
+    private String geographicData;
+    private String DOB;
+    private String telephoneNumber;
+    private String faxNumber;
+    private String emailAddress;
+    private String SSN;
+    private String medicalrecordNumber;
+    private String healthplanbeneficiaryNumber;
+    private String bankaccountNumber;
+    private String certificateNumber;
+    private String vehicleidentifiers;
+    private String deviceidentifiers;
+    private String linkedin;
+    private String ipAddress;
+    private String NUID;
+    private ImageIcon image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeographicData() {
+        return geographicData;
+    }
+
+    public void setGeographicData(String geographicData) {
+        this.geographicData = geographicData;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getMedicalrecordNumber() {
+        return medicalrecordNumber;
+    }
+
+    public void setMedicalrecordNumber(String medicalrecordNumber) {
+        this.medicalrecordNumber = medicalrecordNumber;
+    }
+
+    public String getHealthplanbeneficiaryNumber() {
+        return healthplanbeneficiaryNumber;
+    }
+
+    public void setHealthplanbeneficiaryNumber(String healthplanbeneficiaryNumber) {
+        this.healthplanbeneficiaryNumber = healthplanbeneficiaryNumber;
+    }
+
+    public String getBankaccountNumber() {
+        return bankaccountNumber;
+    }
+
+    public void setBankaccountNumber(String bankaccountNumber) {
+        this.bankaccountNumber = bankaccountNumber;
+    }
+
+    public String getCertificateNumber() {
+        return certificateNumber;
+    }
+
+    public void setCertificateNumber(String certificateNumber) {
+        this.certificateNumber = certificateNumber;
+    }
+
+    public String getVehicleidentifiers() {
+        return vehicleidentifiers;
+    }
+
+    public void setVehicleidentifiers(String vehicleidentifiers) {
+        this.vehicleidentifiers = vehicleidentifiers;
+    }
+
+    public String getDeviceidentifiers() {
+        return deviceidentifiers;
+    }
+
+    public void setDeviceidentifiers(String deviceidentifiers) {
+        this.deviceidentifiers = deviceidentifiers;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getNUID() {
+        return NUID;
+    }
+
+    public void setNUID(String NUID) {
+        this.NUID = NUID;
+    }
+
+    public ImageIcon getImage() {
+        return image;
+    }
+
+    public void setImage(ImageIcon image) {
+        this.image = image;
+    }
+     
+}
