8c5e200cfeb08d9f389ec33c08a9c3708433008a
vinitharsora <harsora.v@northeastern.edu>
2021-11-17 19:45:11 -0500

    Check

---
+++ Test/src/Business/Restaurant/Dishes.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Restaurant;
+
+/**
+ *
+ * @author mitalii
+ */
+public class Dishes {
+    private String name;
+    private String price;
+    private String description;
+
+    public Dishes(String name,String description,String price) {
+        this.name=name;
+        this.price=price;
+        this.description=description;
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    @Override
+    public String toString() {
+        return name;
+    }
+}
